38809e41106a7feea8339d7b08d303aca135c33c
Ryan Taylor <ryanmt@byu.net>
2013-12-20 21:50:32 -0700

    working on getting yamls that work, since the comment string exists in our SMARTS strings

--- lib/lipid_classifier.rb
+++ lib/lipid_classifier.rb
@@ -1,5 +1,10 @@
 Dir.glob(File.dirname(__FILE__) + "*.rb").map {|f| require f }
 
 class LipidClassifier
+  # Load the SMARTS
+  require 'yaml'
+  SMARTS = YAML.load_file("smart_search_strings.yml")
+
+  p SMARTS
 
 end
